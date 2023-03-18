5ed4eecce9b29902c4fb649f963842163da0fbdc
mwoio <126999754+mwoio@users.noreply.github.com>
2023-03-18 20:42:41 +0530

    Update song.py

--- AlexaMusic/utils/inline/song.py
+++ AlexaMusic/utils/inline/song.py
@@ -27,7 +27,7 @@ def song_markup(_, vidid):
         [
             InlineKeyboardButton(
                 text="• sᴜᴩᴩᴏʀᴛ •",
-                url="https://t.me/DevilsHeavenMF",
+                url="https://t.me/verified_noob",
             ),
             InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
         ],
