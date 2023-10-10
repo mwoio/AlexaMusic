d86809bdee7a670942cff3e2ddcbbd57842695f6
mwoio <126999754+mwoio@users.noreply.github.com>
2023-10-10 18:42:03 +0530

    Update call.py

--- AlexaMusic/core/call.py
+++ AlexaMusic/core/call.py
@@ -46,7 +46,7 @@ from AlexaMusic.utils.exceptions import AssistantErr
 from AlexaMusic.utils.inline.play import stream_markup
 from AlexaMusic.utils.stream.autoclear import auto_clean
 from AlexaMusic.utils.thumbnails import gen_thumb
-from strings import get_string
+from AlexaMusic.utils.theme import check_theme
 
 autoend = {}
 counter = {}
