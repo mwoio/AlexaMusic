4b5a57ef639927234dcb4a60483e896285b51b36
mwoio <126999754+mwoio@users.noreply.github.com>
2023-10-10 18:23:06 +0530

    Update bot.py

--- AlexaMusic/core/bot.py
+++ AlexaMusic/core/bot.py
@@ -43,7 +43,7 @@ class AlexaBot(Client):
             )
             sys.exit()
         a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
-        if a.status != "administrator":
+        if a.status != ChatMemberStatus.ADMINISTRATOR
             LOGGER(__name__).error("Please promote Bot as Admin in Logger Group")
             sys.exit()
         if get_me.last_name:
